e25fffea48f4b7913481aa8989d342b2a07ca8b1
ahagopian26 <ahagopian26@students.polytechnic.org>
2026-02-17 14:09:01 -0800

    Update PlaylistTester.java

--- PlaylistProject/PlaylistTester.java
+++ PlaylistProject/PlaylistTester.java
@@ -1,17 +1,19 @@
+package PlaylistProject;
+
 public class PlaylistTester {
     public static void main(String[] args) {
 
-        System.out.println("Initializing a Playlist...\n");
         Playlist p = new Playlist();
 
         System.out.println("Adding songs to the Playlist...\n");
+
         p.addSong(new Song("This Must Be the Place", "Talking Heads", 296));
         p.addSong(new Song("Dominoes", "Donald Byrd", 273));
         p.addSong(new Song("Check the Rhime", "A Tribe Called Quest", 216));
         p.addSong(new Song("Con Altura", "Rosalia", 161));
         p.addSong(new Song("California", "Joni Mitchell", 230));
 
-        System.out.println("Printing the songs...\n");
+        System.out.println("\nPrinting the songs...\n");
         p.printSongs();
 
         System.out.println("\nLiking the songs in position 1, 3 and 4...\n");
@@ -19,23 +21,23 @@ public class PlaylistTester {
         p.likeSong(3);
         p.likeSong(4);
 
-        System.out.println("Printing the songs...\n");
+        System.out.println("\nPrinting the songs...\n");
         p.printSongs();
 
         System.out.println("\nRemoving the song in position 2...\n");
         p.removeSong(2);
 
-        System.out.println("Printing the songs...\n");
+        System.out.println("\nPrinting the songs...\n");
         p.printSongs();
 
         System.out.println("\nPrinting only the liked songs...\n");
         p.printLikedSongs();
 
         System.out.println("\nPrinting the total duration of all songs...");
-        System.out.println(p.totalDuration());
+        System.out.println(p.getTotalDuration());
 
         System.out.println("\nRemoving all unliked songs at once...\n");
-        p.removeUnliked();
+        p.removeAllUnlikedSongs();
 
         System.out.println("Printing all songs...\n");
         p.printSongs();
